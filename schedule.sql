c6d62d3e384d3dda8880ea42b54c9bfe3d048b8a
이현수 <mytime0316@naver.com>
2025-02-07 20:19:15 +0900

    SQL 작성

--- schedule.sql
+++ schedule.sql
@@ -3,9 +3,9 @@ use schedule_jpa;
 -- 테이블 생성
 CREATE TABLE schedule (
                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          task VARCHAR(255) NOT NULL,
-                          name VARCHAR(255) NOT NULL,
-                          password VARCHAR(255) NOT NULL,
+                          user_name VARCHAR(255) NOT NULL,
+                          title VARCHAR(255) NOT NULL,
+                          contents VARCHAR(255) NOT NULL,
                           created_at DATETIME NOT NULL,
                           updated_at TIMESTAMP NOT NULL
 );
