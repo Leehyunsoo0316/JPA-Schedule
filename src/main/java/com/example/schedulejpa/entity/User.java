4580f26612db1d66d7055a5a9a52fc2bc5d99637
이현수 <mytime0316@naver.com>
2025-02-10 16:57:37 +0900

    Lv2 User Entity 개발

---
+++ src/main/java/com/example/schedulejpa/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.schedulejpa.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "user")
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
